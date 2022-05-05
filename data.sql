3f8957f100acb50dacbe04c5f6bc4d883d3513f8
usorfaitheloho <usorfaitheloho@gmail.com>
2022-05-04 17:17:58 -0700

    Set species to digimon where name ends in mon

--- data.sql
+++ data.sql
@@ -32,3 +32,12 @@ VALUES ('Blossom','1998-10-13',3,true,17);
 
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) 
 VALUES ('Ditto','2022-05-14',4,true,22);
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
