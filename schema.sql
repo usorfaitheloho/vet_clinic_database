1ccaae65e3fa43bdb6a9a5b7e7a1324d5f81a2a3
usorfaitheloho <usorfaitheloho@gmail.com>
2022-05-07 11:13:04 -0700

    Add vets table

--- schema.sql
+++ schema.sql
@@ -8,27 +8,29 @@ CREATE TABLE animals (
     neutered BOOLEAN NOT NULL,
     weight_kg DECIMAL NOT NULL,
     PRIMARY KEY(id) 
-    );
+);
 
-    ALTER TABLE animals ADD COLUMN species VARCHAR(20);
+ ALTER TABLE animals ADD COLUMN species VARCHAR(20);
 
-    CREATE TABLE owners(
+CREATE TABLE owners(
     id INT GENERATED ALWAYS AS IDENTITY, 
     full_name VARCHAR(100) NOT NULL,
     age SMALLINT NOT NULL,
     PRIMARY KEY(id)
-    );
+);
 
-    CREATE TABLE species (
+CREATE TABLE species (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY(id)
-    );
+);
 
-    ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals DROP COLUMN species;
 
-    ALTER TABLE animals ADD species_id INTEGER;
-    ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
+ALTER TABLE animals ADD species_id INTEGER;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
 
-    ALTER TABLE animals ADD owner_id INTEGER;
-    ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
+ALTER TABLE animals ADD owner_id INTEGER;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
+
+CREATE TABLE vets(id INT GENERATED ALWAYS AS IDENTITY,NAME VARCHAR(100),AGE INT,date_of_graduation DATE);
